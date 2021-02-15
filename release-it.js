e4c03c1b56f5b8005394b92cca576e7d51f377ce
Jakub Mazurek <jakub.mazurek.dev@gmail.com>
2021-02-15 22:02:56 +0100

    chore: add release-it hooks

---
+++ release-it.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "hooks": {
+    "before:init": "npm run checks",
+    // Docs are generated using build CLI, so we have to build first
+    "after:bump": ["npm run build", "npm run docs-gen"]
+  },
+  "git": {
+    "commitMessage": "chore: release v${version}",
+    "commit": true,
+    "tag": true,
+    "push": true,
+  },
+  "github": {
+    "release": true
+  }
+}
